61deb504b91d2a16655b00a02574ede7e3614d9f
usongame <3239857092@qq.com>
2020-09-05 18:13:33 +0800

    fix some bug

--- registration.php
+++ registration.php
@@ -99,7 +99,7 @@ $s="select * from bloodgroup";
 <tr><td class="lefttd">密码</td><td><input type="password" name="t6" required="required" pattern="[a-zA-Z0-9]{2,10}" title="please enter only character or numbers between 2 to 10 for password" /></td></tr>
 <tr><td class="lefttd">确认密码</td><td><input type="password" name="t7" required="required" pattern="[a-zA-Z0-9 ]{2,10}" title="please enter only character or numbers between 2 to 10 for password" /></td></tr>
 
-<tr><td class="lefttd">上传头像</td><td><input type="file" name="t8" /></td></tr>
+<!-- <tr><td class="lefttd">上传头像</td><td><input type="file" name="t8" /></td></tr> -->
 <tr><td>&nbsp;</td><td><input type="submit" value="注册" name="sbmt" style="border:0px; background:linear-gradient(#900,#D50000); width:100px; height:30px; border-radius:10px 1px 10px 1px; box-shadow:1px 1px 5px black; color:white; font-weight:bold; font-size:14px; text-shadow:1px 1px 6px black; "></td></tr></table>
 </td></tr>
 </table>
@@ -116,50 +116,50 @@ $s="select * from bloodgroup";
 <?php
 if(isset($_POST["sbmt"])) 
 {
-$target_dir = "doner_pic/";
-$target_file = $target_dir . basename($_FILES["t8"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// $target_dir = "doner_pic/";
+// $target_file = $target_dir . basename($_FILES["t8"]["name"]);
+// $uploadOk = 1;
+// $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 
-    $check = getimagesize($_FILES["t8"]["tmp_name"]);
-    if($check !== false) {
-      //  echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "文件不是图片.";
-        $uploadOk = 0;
-    }
+    // $check = getimagesize($_FILES["t8"]["tmp_name"]);
+    // if($check !== false) {
+    //   //  echo "File is an image - " . $check["mime"] . ".";
+    //     $uploadOk = 1;
+    // } else {
+    //     echo "文件不是图片.";
+    //     $uploadOk = 0;
+    // }
 
 // Check if file already exists
-if (file_exists($target_file)) {
-    echo "文件已经存在.";
-    $uploadOk = 0;
-}
+// if (file_exists($target_file)) {
+//     echo "文件已经存在.";
+//     $uploadOk = 0;
+// }
 //aloow certain file formats
-	if($imageFileType != "jpg" && $imageFileType !="png" && $imageFileType !="jpeg" && $imageFileType !="gif"){
-		echo "只接受 jpg, jpeg, Png & gif 格式文件.";
-		$uploadok=0;
-	}else{
-		if(move_uploaded_file($_FILES["t8"]["tmp_name"], $target_file)){
+	// if($imageFileType != "jpg" && $imageFileType !="png" && $imageFileType !="jpeg" && $imageFileType !="gif"){
+	// 	echo "只接受 jpg, jpeg, Png & gif 格式文件.";
+	// 	$uploadok=0;
+	// }else{
+		// if(move_uploaded_file($_FILES["t8"]["tmp_name"], $target_file)){
 		$cn=makeconnection();
-			$s="insert into donarregistration(name,gender,age,mobile,b_id,email,pwd,pic) values('" . $_POST["t1"] ."','" . $_POST["r1"] . "','" . $_POST["t2"] . "','" . $_POST["t3"] . "','" . $_POST["t4"] . "','" . $_POST["t5"] . "','" . $_POST["t6"] .  "','" . basename($_FILES["t8"]["name"])  ."')";
+			$s="insert into donarregistration(name,gender,age,mobile,b_id,email,pwd,pic) values('" . $_POST["t1"] ."','" . $_POST["r1"] . "','" . $_POST["t2"] . "','" . $_POST["t3"] . "','" . $_POST["t4"] . "','" . $_POST["t5"] . "','" . $_POST["t6"] .  "','" . '' ."')";
 			
 			//$s="INSERT INTO donarregistration(name,gender,age,mobile,b_id,email,pswd,pic) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9])"
 	mysqli_query($cn,$s);
 	mysqli_close($cn);
-	if($s>0)
-	{
+	// if($s>0)
+	// {
 	
-	}
-	else
-	{
-	}
-		} else{
-			echo "上传图片失败.";
-		}	
+	// }
+	// else
+	// {
+	// }
+	// 	} else{
+	// 		echo "上传图片失败.";
+	// 	}	
 	
-	}
+	// }
 }
 ?> 
 </body>
